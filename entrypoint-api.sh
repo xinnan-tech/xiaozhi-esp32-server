c4b638c97be06deefa8b7b7d974be8e73997117b
lizhongxiang <lzx@qq.com>
2025-02-26 14:41:57 +0800

    add: 部署文件

--- entrypoint-api.sh
+++ entrypoint-api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 检查 asr-models/SenseVoiceSmall 目录是否存在
-if [ ! -d "asr-models/SenseVoiceSmall" ]; then
+if [ ! -d "asr-models/iic/SenseVoiceSmall" ]; then
     # 如果目录不存在，执行下载脚本
     python modelscope_model_download.py
 fi
