ab45342349474e53e5e276f35a3ad73f5ce29b32
vonweller <154695311+vonweller@users.noreply.github.com>
2025-03-05 14:54:31 +0800

    视觉识别添加是否是Openai协议的判断
    视觉识别添加是否是Openai协议的判断，当前测试openai全系使用无问题

--- core/handle/VLHandle.py
+++ core/handle/VLHandle.py
@@ -4,8 +4,12 @@ from config.logger import setup_logging
 logger = setup_logging()
 
 async def handleVLMessage(conn, text):
-    messages =  [
-        {"type": "image_url","image_url": {"url": f"data:image/png;base64,{text}"},},
-        {"type": "text", "text": "图中描绘的是什么景象,请细致查看并描述"},
-    ]
+    #print(text)
+    messages =[]
+    if conn.config["LLM"][conn.config["selected_module"]["LLM"]]['type']=='openai':
+        #print('当前openai接口协议LLM')
+        messages = [
+            {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{text}"}, },
+            {"type": "text", "text": "图中描绘的是什么景象,请细致查看并描述"},
+        ]
     conn.executor.submit(conn.chat, messages)
