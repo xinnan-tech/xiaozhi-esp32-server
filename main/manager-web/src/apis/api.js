801758e70b9cb3195a0463c6d42f679b4166ac35
CGD <3030332422@qq.com>
2025-03-07 18:21:15 +0800

    获取用户信息和已绑设备

--- main/manager-web/src/apis/api.js
+++ main/manager-web/src/apis/api.js
@@ -1,5 +1,6 @@
 // 引入各个模块的请求
 import user from './module/user.js'
+
 /**
  * 接口地址
  * 在开发阶段，如果地址写的是相对路径，请与vue.config.js的devServer配置相结合，方便跨域请求
@@ -15,8 +16,16 @@ export function getServiceUrl () {
   return DEV_API_SERVICE
 }
 
+const Api = {
+    getServiceUrl,
+    user
+};
+
+
 /** request服务封装 */
 export default {
   getServiceUrl,
-  user
+  user,
+  Api,
 }
+
