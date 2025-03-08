35f5a9c6e3ba034047c98bac2303a9e4cf4ec720
hrz <1710360675@qq.com>
2025-03-08 10:58:49 +0800

    update:测试新版镜像自动化编译

--- docs/docker/start.sh
+++ docs/docker/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 启动Java后端（监听8003端口）
+# 启动Java后端（docker内监听8003端口）
 java -jar /app/xiaozhi-esp32-api.jar \
   --server.port=8003 \
   --spring.datasource.druid.url=${SPRING_DATASOURCE_DRUID_URL} \
