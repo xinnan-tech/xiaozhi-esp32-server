1cb52c8e052a7243cd911c54b1976257a44a1ff9
Ken <ulxiping@qq.com>
2025-03-12 09:27:43 +0800

    Register 风格统一

--- main/manager-web/src/router/index.js
+++ main/manager-web/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Welcome from '../views/welcome.vue'
 import Login from '../views/login.vue'
-import register from '@/views/register.vue' 
+import Register from '@/views/register.vue' 
 
 Vue.use(VueRouter)
 
@@ -31,7 +31,7 @@ const routes = [
   },
   {
     path: '/register',
-    name: 'register',
+    name: 'Register',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
