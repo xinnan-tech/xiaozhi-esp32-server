3856f78fa25341a447fb5afff2d952520aaca0f5
seven <wantt2@qq.com>
2025-03-27 11:33:53 +0800

    add

--- main/xiaozhi-server/core/providers/tts/linkerai.py
+++ main/xiaozhi-server/core/providers/tts/linkerai.py
@@ -8,7 +8,6 @@ import os
 from pydub import AudioSegment
 import opuslib_next
 import numpy as np
-from collections import deque
 
 class TTSProvider(TTSProviderBase):
     def __init__(self, config, delete_audio_file):
@@ -18,10 +17,10 @@ class TTSProvider(TTSProviderBase):
         self.response_format = config.get("response_format")
         self.sample_rate = config.get("sample_rate")
         self.instruct_text = config.get("instruct_text")
+        self.stream_mode = config.get('stream_mode') # stream or double_stream
         self.host = "tts.linkerai.top"
         self.api_url = 'http://tts.linkerai.top:24003/tts'
-        self.cache_queue = deque(maxlen=30)
-
+        
     def generate_filename(self, extension=".wav"):
         return os.path.join(self.output_file, f"tts-{datetime.now().date()}@{uuid.uuid4().hex}{extension}")
 
@@ -41,6 +40,7 @@ class TTSProvider(TTSProviderBase):
         }
         
         with open(output_file,'w',encoding='utf-8') as f:
+            f.write('http_post\n')
             f.write('%s\n'%(json.dumps(params,ensure_ascii=False)))
             f.write('%s'%(json.dumps(headers,ensure_ascii=False)))
         self.cache_queue.append(output_file)
@@ -57,37 +57,42 @@ class TTSProvider(TTSProviderBase):
             print(f"错误信息: {response.text}")
 
     def audio_to_opus_data(self, audio_file_path):
-        if audio_file_path in self.cache_queue:# os.path.exists('%s.linkerai'%audio_file_path):
-            duration = 100
-            code = []
+        data = b''
+        if os.path.exists('%s.linkerai'%audio_file_path):
             with open(audio_file_path,encoding='utf-8') as f:
-                for line in f.readlines():
-                    code.append(json.loads(line.strip()))
-            params = code[0]
-            headers = code[1]
-            return self.yield_data(params=params,headers=headers),duration
-        else: # 兼容非流式音频播放
-            file_type = os.path.splitext(audio_file_path)[1]
-            if file_type:
-                file_type = file_type.lstrip('.')
-            audio = AudioSegment.from_file(audio_file_path, format=file_type, parameters=["-nostdin"])
+                data = f.read(10)
+                if data.startswith('http_post'):
+                    duration = 100
+                    code = []
+                    for line in f.readlines():
+                        code.append(json.loads(line.strip()))
+                    params = code[0]
+                    headers = code[1]
+                    return self.yield_data(params=params,headers=headers),duration
+                
+            if not data.startswith('http_post'):# 兼容非流式音频播放
+                f.seek(0)
+                file_type = os.path.splitext(audio_file_path)[1]
+                if file_type:
+                    file_type = file_type.lstrip('.')
+                audio = AudioSegment.from_file(audio_file_path, format=file_type, parameters=["-nostdin"])
 
-            audio = audio.set_channels(1).set_frame_rate(16000).set_sample_width(2)
-            duration = len(audio) / 1000.0
-            raw_data = audio.raw_data
-            encoder = opuslib_next.Encoder(16000, 1, opuslib_next.APPLICATION_AUDIO)
-            frame_duration = 60  # 60ms per frame
-            frame_size = int(16000 * frame_duration / 1000)  # 960 samples/frame
+                audio = audio.set_channels(1).set_frame_rate(16000).set_sample_width(2)
+                duration = len(audio) / 1000.0
+                raw_data = audio.raw_data
+                encoder = opuslib_next.Encoder(16000, 1, opuslib_next.APPLICATION_AUDIO)
+                frame_duration = 60  # 60ms per frame
+                frame_size = int(16000 * frame_duration / 1000)  # 960 samples/frame
 
-            opus_datas = []
-            for i in range(0, len(raw_data), frame_size * 2):  # 16bit=2bytes/sample
-                chunk = raw_data[i:i + frame_size * 2]
-                if len(chunk) < frame_size * 2:
-                    chunk += b'\x00' * (frame_size * 2 - len(chunk))
-                np_frame = np.frombuffer(chunk, dtype=np.int16)
-                opus_data = encoder.encode(np_frame.tobytes(), frame_size)
-                opus_datas.append(opus_data)
-            return opus_datas, duration
+                opus_datas = []
+                for i in range(0, len(raw_data), frame_size * 2):  # 16bit=2bytes/sample
+                    chunk = raw_data[i:i + frame_size * 2]
+                    if len(chunk) < frame_size * 2:
+                        chunk += b'\x00' * (frame_size * 2 - len(chunk))
+                    np_frame = np.frombuffer(chunk, dtype=np.int16)
+                    opus_data = encoder.encode(np_frame.tobytes(), frame_size)
+                    opus_datas.append(opus_data)
+                return opus_datas, duration
         
 if __name__ == "__main__":
     config = {'access_token':'123',
