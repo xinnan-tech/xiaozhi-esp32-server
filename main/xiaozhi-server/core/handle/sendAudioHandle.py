08cb567f0686c0356f2158ba726e44d77797bfc0
seven <wantt2@qq.com>
2025-03-29 18:39:36 +0800

    add

--- main/xiaozhi-server/core/handle/sendAudioHandle.py
+++ main/xiaozhi-server/core/handle/sendAudioHandle.py
@@ -43,7 +43,8 @@ async def sendAudioStream(conn, audios):
         async for opus_packet in audios:
             conn.logger.bind(tag=TAG).info(f'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx,{type(opus_packet)},{opus_packet}')
             if isinstance(opus_packet,bytes) and opus_packet.startswith(b'text:'):
-                await send_tts_message(conn, "sentence_start", opus_packet.replace(b'text:','').decode('utf-8'))
+                # await send_tts_message(conn, "sentence_start", opus_packet.replace(b'text:','').decode('utf-8'))
+                await send_tts_message(conn, "sentence_start", opus_packet)
                 continue
             if len(opus_packet) < 1:
                 continue
