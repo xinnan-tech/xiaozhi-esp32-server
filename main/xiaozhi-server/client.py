74d27bc84c06ab710a5a1fcd43bf191e5adad1e0
rahulpscraftech360 <rahul@craftech360.com>
2025-08-18 10:10:23 +0000

    working tenvad

--- main/xiaozhi-server/client.py
+++ main/xiaozhi-server/client.py
@@ -19,9 +19,9 @@ import opuslib
 
 # --- Configuration ---
 
-SERVER_IP = "192.168.1.102" # !!! UPDATE with your server's local IP address !!!
+SERVER_IP = "64.227.170.31" # !!! UPDATE with your server's local IP address !!!
 OTA_PORT = 8003
-MQTT_BROKER_HOST = "192.168.1.102"  # MQTT gateway IP
+MQTT_BROKER_HOST = "64.227.170.31"  # MQTT gateway IP
 
 
 MQTT_BROKER_PORT = 1883
